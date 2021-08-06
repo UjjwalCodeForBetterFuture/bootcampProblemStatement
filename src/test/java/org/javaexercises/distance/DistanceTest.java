262942bd51190e6d1f85477fb32fe2058621f1c9
Ujjwal Deswal <Ujjwal_Deswal@mckinsey.com>
2021-08-06 11:58:40 +0530

    Ujjwal|Saumya Distance Problem

--- src/test/java/org/javaexercises/distance/DistanceTest.java
+++ src/test/java/org/javaexercises/distance/DistanceTest.java
@@ -7,8 +7,8 @@ public class DistanceTest {
 
     @Test
     public void thousandMetersShouldEqualThousandMeters() {
-        Distance distance1000M = new Distance(1000, "meter");
-        Distance distance1000MCopy = new Distance(1000, "meter");
+        Distance distance1000M = new Distance(1000, DistanceEnum.METER.name());
+        Distance distance1000MCopy = new Distance(1000, DistanceEnum.METER.name());
         Assertions.assertTrue(distance1000M.equals(distance1000MCopy));
     }
 
@@ -22,22 +22,22 @@ public class DistanceTest {
 
     @Test
     public void distance_compare_metre_to_kilometer() {
-        Distance distance1000M = new Distance(1000, "meter");
-        Distance distance1KM = new Distance(1, "kilometer");
+        Distance distance1000M = new Distance(1000, DistanceEnum.METER.name());
+        Distance distance1KM = new Distance(1, DistanceEnum.KILOMETERS.name());
         Assertions.assertTrue(distance1000M.equals(distance1KM));
     }
 
     @Test
     public void distance_compare_metre_to_kilometer_with_different_unit() {
-        Distance distance1000M = new Distance(1000, "meter");
-        Distance distance2KM = new Distance(2, "kilometer");
+        Distance distance1000M = new Distance(1000, DistanceEnum.METER.name());
+        Distance distance2KM = new Distance(2, DistanceEnum.KILOMETERS.name());
         Assertions.assertFalse(distance1000M.equals(distance2KM));
     }
 
     @Test
     public void distance_compare_metre_to_meter_with_different_unit() {
-        Distance distance1M = new Distance(1, "meter");
-        Distance distance2M = new Distance(2, "meter");
+        Distance distance1M = new Distance(1, DistanceEnum.METER.name());
+        Distance distance2M = new Distance(2, DistanceEnum.METER.name());
         Assertions.assertFalse(distance1M.equals(distance2M));
     }
 
